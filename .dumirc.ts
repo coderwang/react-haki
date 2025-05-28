a597097910860bc9c637c2e2d516d08d57267936
wangshuhan <wangshuhan@kanzhun.com>
2025-05-28 17:16:22 +0800

    Updates

---
+++ .dumirc.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  outputPath: 'docs-dist',
+  favicons: ['/favicon.ico'],
+  themeConfig: {
+    name: 'react haki',
+    logo: '/logo.png',
+    footer:
+      '<strong>React Haki</strong> | Open-source MIT Licensed | Powered by <a href="https://d.umijs.org/" target="_blank">dumi</a> 😘',
+  },
+  resolve: {
+    atomDirs: [
+      { type: 'component', dir: 'src/components' },
+      { type: 'hook', dir: 'src/hooks' },
+    ],
+  },
+});
