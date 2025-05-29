a971d98f79044eab38131a216db6f9ee927251a4
coderwang <coderwang@users.noreply.github.com>
2025-05-29 12:46:56 +0000

    deploy: ab89082f108fa869ffb2ab3d461a9fc49d0d0c46

---
+++ docs__guide__initialize.md.197b6941.async.js
@@ -0,0 +1,12 @@
+"use strict";(self.webpackChunkreact_haki=self.webpackChunkreact_haki||[]).push([[619],{97256:function(t,n,_){_.r(n);var a=_(72269),l=_(93359),o=_(61788),m=_(19977),c=_(78003),d=_(24268),E=_(96057),x=_(83213),r=_(53683),u=_(80936),s=_(67294),i=_(38016),e=_(85893);function h(){return(0,e.jsx)(r.dY,{children:(0,e.jsx)(s.Suspense,{fallback:(0,e.jsx)(u.Z,{}),children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h1",{id:"\u5FEB\u901F\u5F00\u59CB",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5FEB\u901F\u5F00\u59CB",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u5FEB\u901F\u5F00\u59CB"]}),(0,e.jsxs)("h2",{id:"\u5B89\u88C5\u4F9D\u8D56",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5B89\u88C5\u4F9D\u8D56",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u5B89\u88C5\u4F9D\u8D56"]}),(0,e.jsx)(d.Z,{lang:"bash",children:i.texts[0].value}),(0,e.jsxs)("h2",{id:"\u4F7F\u7528",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u4F7F\u7528",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u4F7F\u7528"]}),(0,e.jsx)(d.Z,{lang:"js",children:i.texts[1].value})]})})})})}n.default=h},38016:function(t,n,_){_.r(n),_.d(n,{texts:function(){return a}});const a=[{value:`# \u901A\u8FC7 npm \u5B89\u88C5
+npm i react-haki
+
+# \u901A\u8FC7 yarn \u5B89\u88C5
+yarn add react-haki
+
+# \u901A\u8FC7 pnpm \u5B89\u88C5
+pnpm add react-haki
+`,paraId:0,tocIndex:1},{value:`import { Empty } from 'react-haki';
+
+export default () => <Empty />;
+`,paraId:1,tocIndex:2}]}}]);
--- docs__guide__initialize.md.72e91dc1.async.js
+++
@@ -1,7 +0,0 @@
-"use strict";(self.webpackChunkreact_haki=self.webpackChunkreact_haki||[]).push([[619],{97256:function(t,n,_){_.r(n);var a=_(72269),o=_(93359),l=_(61788),m=_(19977),c=_(78003),i=_(24268),E=_(96057),x=_(83213),u=_(53683),r=_(80936),s=_(67294),d=_(38016),e=_(85893);function h(){return(0,e.jsx)(u.dY,{children:(0,e.jsx)(s.Suspense,{fallback:(0,e.jsx)(r.Z,{}),children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h1",{id:"\u5FEB\u901F\u5F00\u59CB",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5FEB\u901F\u5F00\u59CB",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u5FEB\u901F\u5F00\u59CB"]}),(0,e.jsxs)("h2",{id:"\u5B89\u88C5\u4F9D\u8D56",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5B89\u88C5\u4F9D\u8D56",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u5B89\u88C5\u4F9D\u8D56"]}),(0,e.jsx)(i.Z,{lang:"bash",children:d.texts[0].value}),(0,e.jsxs)("h2",{id:"\u5F15\u5165",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5F15\u5165",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u5F15\u5165"]}),(0,e.jsx)(i.Z,{lang:"bash",children:d.texts[1].value})]})})})})}n.default=h},38016:function(t,n,_){_.r(n),_.d(n,{texts:function(){return a}});const a=[{value:`# \u901A\u8FC7 npm \u5B89\u88C5
-npm i react-haki
-
-# \u901A\u8FC7 yarn \u5B89\u88C5
-yarn add react-haki
-`,paraId:0,tocIndex:1},{value:`import { Button } from 'react-haki';
-`,paraId:1,tocIndex:2}]}}]);
